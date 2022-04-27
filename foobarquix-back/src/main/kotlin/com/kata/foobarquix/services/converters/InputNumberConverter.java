a8e4f318018f343dea8f4206f727d6aabe69e249
ET35272 <karim.ayite@bred.fr>
2022-04-27 16:15:16 +0200

    Refactor

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/converters/InputNumberConverter.java
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/converters/InputNumberConverter.java
@@ -7,31 +7,38 @@ public class InputNumberConverter {
     public static final String FOO = "Foo";
     public static final String BAR = "Bar";
     public static final String QUIX = "Quix";
+    private static final int FOO_NUMBER = 3;
+    private static final int BAR_NUMBER = 5;
+    private static final int QUIX_NUMBER = 7;
 
-    List<Integer> convertableNumbersList = Collections.unmodifiableList(Arrays.asList(3, 5, 7));
+    List<Integer> convertableNumbersList = Collections.unmodifiableList(Arrays.asList(FOO_NUMBER, BAR_NUMBER, QUIX_NUMBER));
 
     public String convertNumber(int inputNumber) {
-        List<NumberConverter> numberConverters = initializeConvertersByOrderOfAppearance(inputNumber);
+
+        List<NumberConverter> numberConverters = initializeConvertersByContentOrderOfAppearance(inputNumber);
+
         Optional<String> convertedNumberOptional = numberConverters
                 .stream()
                 .map(numberConverter -> numberConverter.convert(inputNumber))
-                .reduce((s, s2) -> s.concat(s2))
-                .map(convertedNumber -> removeNumberDigits(convertedNumber));
+                .reduce(String::concat)
+                .map(this::removeDigitsFromRowConvertedNumber);
 
         return convertedNumberOptional.orElse(String.valueOf(inputNumber));
     }
 
-    private List<NumberConverter> initializeConvertersByOrderOfAppearance(int inputNumber) {
+    private List<NumberConverter> initializeConvertersByContentOrderOfAppearance(int inputNumber) {
         List<NumberConverter> numbersConverters = new ArrayList<>();
+
         String inputNumberAsString = String.valueOf(inputNumber);
 
-        for (int i = 0; i < convertableNumbersList.size(); i++) {
-            int potentielConvertableNumber = convertableNumbersList.get(i);
-            if (NumberConverter.isDivisable(potentielConvertableNumber, inputNumber)) {
-                addConverter(getDivisableConverter(potentielConvertableNumber), numbersConverters);
-            }
-        }
+        initializeDivisableConverters(inputNumber, numbersConverters);
 
+        initializeContainsConverters(numbersConverters, inputNumberAsString);
+
+        return numbersConverters;
+    }
+
+    private void initializeContainsConverters(List<NumberConverter> numbersConverters, String inputNumberAsString) {
         List<Character> digitsConverted = new ArrayList<>();
 
         for (int i = 0; i < inputNumberAsString.length(); i++) {
@@ -42,16 +49,21 @@ public class InputNumberConverter {
 
                 int potentielConvertableNumber = digit - '0';
 
-                for (int j = 0; j < convertableNumbersList.size(); j++) {
-                    int convertableNumber = convertableNumbersList.get(j);
+                for (int convertableNumber : convertableNumbersList) {
                     if (NumberConverter.contains(potentielConvertableNumber, convertableNumber)) {
                         addConverter(getContainsConverter(potentielConvertableNumber), numbersConverters);
                     }
                 }
             }
         }
+    }
 
-        return numbersConverters;
+    private void initializeDivisableConverters(int inputNumber, List<NumberConverter> numbersConverters) {
+        for (int potentielConvertableNumber : convertableNumbersList) {
+            if (NumberConverter.isDivisable(potentielConvertableNumber, inputNumber)) {
+                addConverter(getDivisableConverter(potentielConvertableNumber), numbersConverters);
+            }
+        }
     }
 
     private boolean isDigitAlreadyHaveHisConverter(List<Character> digitsConverted, char digit) {
@@ -59,33 +71,32 @@ public class InputNumberConverter {
     }
 
     private void addConverter(NumberConverter potentielConvertableNumber, List<NumberConverter> convertableNumbers) {
-        NumberConverter containsConverter = potentielConvertableNumber;
-        if (containsConverter != null) {
-            convertableNumbers.add(containsConverter);
+        if (potentielConvertableNumber != null) {
+            convertableNumbers.add(potentielConvertableNumber);
         }
     }
 
     private NumberConverter getDivisableConverter(Integer convertableNumber) {
-        if (convertableNumber == 3) {
+        if (convertableNumber == FOO_NUMBER) {
             return new DivisableConverter(FOO, convertableNumber);
-        } else if (convertableNumber == 5) {
+        } else if (convertableNumber == BAR_NUMBER) {
             return new DivisableConverter(BAR, convertableNumber);
         }
         return null;
     }
 
     private NumberConverter getContainsConverter(Integer convertableNumber) {
-        if (convertableNumber == 3) {
+        if (convertableNumber == FOO_NUMBER) {
             return new ContainsConverter(FOO, convertableNumber);
-        } else if (convertableNumber == 5) {
+        } else if (convertableNumber == BAR_NUMBER) {
             return new ContainsConverter(BAR, convertableNumber);
-        } else if (convertableNumber == 7) {
+        } else if (convertableNumber == QUIX_NUMBER) {
             return new ContainsConverter(QUIX, convertableNumber);
         }
         return null;
     }
 
-    private String removeNumberDigits(String convertedNumber) {
+    private String removeDigitsFromRowConvertedNumber(String convertedNumber) {
         return convertedNumber.replaceAll("\\d", "");
     }
 
