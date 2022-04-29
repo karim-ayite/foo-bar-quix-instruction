ed22d57e6e8d28f6dc50c5a179cef3ec69028e87
karim-ayite <karim.ayite@gmail.com>
2022-04-29 19:27:45 +0200

    refactore : rename methods

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/converters/ListConverterInitializer.java
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/converters/ListConverterInitializer.java
@@ -7,19 +7,17 @@ import java.util.List;
 
 public class ListConverterInitializer {
 
-    public static final String FOO = "Foo";
-    public static final String BAR = "Bar";
-    public static final String QUIX = "Quix";
+    private static final String FOO = "Foo";
+    private static final String BAR = "Bar";
+    private static final String QUIX = "Quix";
     private static final int FOO_NUMBER = 3;
     private static final int BAR_NUMBER = 5;
     private static final int QUIX_NUMBER = 7;
 
-    List<Integer> convertableNumbersList = Collections.unmodifiableList(Arrays.asList(FOO_NUMBER, BAR_NUMBER, QUIX_NUMBER));
+    private final List<Integer> convertableNumbersList = Collections.unmodifiableList(Arrays.asList(FOO_NUMBER, BAR_NUMBER, QUIX_NUMBER));
 
     public List<NumberConverter> init(int inputNumber) {
         List<NumberConverter> numbersConverters = new ArrayList<>();
-
-
 
         initializeDividableConverters(inputNumber, numbersConverters);
 
@@ -37,21 +35,25 @@ public class ListConverterInitializer {
             if (!isDigitAlreadyHaveHisConverter(digitsConverted, digit)) {
                 digitsConverted.add(digit);
 
-                int potentielConvertableNumber = digit - '0';
+                int potentialContainedNumber = convertDigitToInt(digit);
 
                 for (int convertableNumber : convertableNumbersList) {
-                    if (NumberConverter.contains(potentielConvertableNumber, convertableNumber)) {
-                        addConverter(getContainsConverter(potentielConvertableNumber), numbersConverters);
+                    if (NumberConverter.contains(potentialContainedNumber, convertableNumber)) {
+                        addConverter(getContainsConverter(potentialContainedNumber), numbersConverters);
                     }
                 }
             }
         }
     }
 
+    private int convertDigitToInt(char digit) {
+        return digit - '0';
+    }
+
     private void initializeDividableConverters(int inputNumber, List<NumberConverter> numbersConverters) {
-        for (int potentielConvertableNumber : convertableNumbersList) {
-            if (NumberConverter.isDivisable(potentielConvertableNumber, inputNumber)) {
-                addConverter(getDividableConverter(potentielConvertableNumber), numbersConverters);
+        for (int potentialDividableNumber : convertableNumbersList) {
+            if (NumberConverter.isDivisable(potentialDividableNumber, inputNumber)) {
+                addConverter(getDividableConverter(potentialDividableNumber), numbersConverters);
             }
         }
     }
@@ -60,9 +62,9 @@ public class ListConverterInitializer {
         return digitsConverted.contains(digit);
     }
 
-    private void addConverter(NumberConverter potentielConvertableNumber, List<NumberConverter> convertableNumbers) {
-        if (potentielConvertableNumber != null) {
-            convertableNumbers.add(potentielConvertableNumber);
+    private void addConverter(NumberConverter potentialConvertibleNumber, List<NumberConverter> convertableNumbers) {
+        if (potentialConvertibleNumber != null) {
+            convertableNumbers.add(potentialConvertibleNumber);
         }
     }
 
